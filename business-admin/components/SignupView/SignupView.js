1e316e47e728ac2af3fe91892ee4838cd8147d49
Amit Ranan <ranan.amit@gmail.com>
2017-01-31 00:07:55 -0800

    created SignupView components and signup-view.scss files

---
+++ business-admin/components/SignupView/SignupView.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SignupView extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default SignupView;
