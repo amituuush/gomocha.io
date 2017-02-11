2c5d39b9ffa4992f85bb5a60bd110c48ae230f8d
Amit Ranan <ranan.amit@gmail.com>
2017-02-10 17:01:12 -0800

    added search bar feature

--- business-admin/components/App/App.js
+++ business-admin/components/App/App.js
@@ -3,12 +3,21 @@ import { Link } from 'react-router';
 import { LaunchDarkly } from 'react-launch-darkly';
 import './app.scss';
 
+var user = {
+  "firstName": "Bob",
+  "lastName": "Loblaw",
+  "key": "bob@example.com",
+  "custom": {
+    "groups": "beta_testers"
+  }
+};
+
 var App = React.createClass({
 
   render: function() {
     return (
       <div>
-        <LaunchDarkly clientId={} user={{key: "user key"}}
+        <LaunchDarkly clientId={'587d8ac22f61020941bf5d2a'} user={user}>
           {this.props.children}
         </LaunchDarkly>
       </div>
